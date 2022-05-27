7be4de923aafde687667d6d50e6ab9fbf963f93b
Richard Lawrence <richard.lawrence@simplybusiness.co.uk>
2022-05-27 12:08:47 +0100

    create unit test and method which returns buzz if a number is divisable by 5

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,6 +2,8 @@ class FizzBuzz
   def fizz_or_buzz(number)
     if number % 3 == 0
       'fizz'
+    elsif number % 5 == 0
+      'buzz'
     end
   end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -7,5 +7,10 @@ describe FizzBuzz do
       fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.fizz_or_buzz(3)).to eq('fizz')
     end
+
+    it 'prints \'buzz\' if number is divisable by 5' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.fizz_or_buzz(5)).to eq('buzz')
+    end
   end
 end
